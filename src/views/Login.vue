8d1e1707376a394b47e18cad3348ee11f50bc159
JoseAndres-py <josean.lopezm@gmail.com>
2020-12-14 21:00:52 -0500

    Agregar esqueleto login VUE

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login">
+        <auth-login></auth-login>
+    </div>
+</template>
+
+<script>
+
+import AuthLogin from '@/components/auth/AuthLogin.vue'
+
+export default {
+    components:{
+        AuthLogin
+    }
+}
+</script>
